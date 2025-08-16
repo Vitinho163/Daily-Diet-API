92db2dc2ce3c53cfb0871138786d034147a5e557
João Victor <victor.jvsb18@gmail.com>
2025-08-16 16:57:26 -0300

    feat: configure server to use environment port and host

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
-    port: 3333,
+    host: '0.0.0.0',
+    port: env.PORT,
   })
   .then(() => {
     console.log(`HTTP server running!`)
